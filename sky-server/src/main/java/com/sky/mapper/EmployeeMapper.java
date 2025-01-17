19e6d07ddbf494e3368781b3e35e96bb61b2d321
Rue <rokki1021@163.com>
2025-01-17 20:49:15 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +16,12 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     * 插入员工数据
+     * @param employee
+     */
+    //字段用的下划线命名，属性用的驼峰命名，我们的yml文件已经设置了驼峰命名，所以可以传输过去
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time,create_user, update_user, status)" +
+            "values" + " (#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status}) ")
+    void insert(Employee employee);
 }
