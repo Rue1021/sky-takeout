2e503351b4a01b447bf13c148fdbabc7386edc40
Rue <rokki1021@163.com>
2025-02-17 14:31:13 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,28 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    /**
+     * 动态条件查询
+     * @param shoppingCart
+     * @return
+     */
+    //动态查询，可能返回多个数据，所以选择List来接收
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入一条购物车数据
+     * @param shoppingCart
+     */
+    void insert(ShoppingCart shoppingCart);
+}
